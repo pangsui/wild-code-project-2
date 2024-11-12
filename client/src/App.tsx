2544282b5dbe683753bac3f83f0de110e1c4e187
rrczt974 <raphaelriviere87@gmail.com>
2024-11-12 19:31:13 +0100

    [feat] : footer composant & media query mobile and desktop

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,26 @@
 import Card from "./components/Card";
 import "./App.css";
+import CardDev from "./components/CardDev";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Search from "./components/Search";
-import CardDev from "./components/CardDev";
-
-
 
 function App() {
   return (
-    <>  
-    <header className="header">
-      <Navbar />
-      <Search />
+    <>
+      <header className="header">
+        <Navbar />
+        <Search />
       </header>
-    <main className="container">
-      <Card />
-      <CardDev />
-    </main>
+      <main className="container">
+        <Card />
+        <CardDev />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </>
-    );
+  );
 }
 
 export default App;
