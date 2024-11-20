b1f72c00bb81f8c4cec09466ebfa1618a9bb305b
rrczt974 <raphaelriviere87@gmail.com>
2024-11-20 20:10:35 +0100

    [fix] : update header CSS with 3 navigation buttons @ update CSS mobile first

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -12,6 +12,9 @@ export default function Navbar() {
           {" "}
           <h1> HERODEX</h1>
         </NavLink>
+      </div>
+
+      <nav className={styles.navContent}>
         <div className={styles.allSections}>
           <NavLink to="/details" className={styles.link}>
             Explore heroes!
@@ -19,23 +22,23 @@ export default function Navbar() {
           <NavLink to="/allHeroes" className={styles.link}>
             All super heroes
           </NavLink>
-          <NavLink to="/fevorites" className={styles.link}>
-            Fevorites
+          <NavLink to="/favorites" className={styles.link}>
+            Favorites
           </NavLink>
         </div>
-      </div>
-      <nav className="navContent">
-        <button type="button" className={styles.bookmark}>
-          <svg
-            aria-hidden="true"
-            viewBox="0 0 24 24"
-            width="24"
-            height="24"
-            fill="currentColor"
-          >
-            <path d="M6 2c-1.1 0-2 .9-2 2v18l8-4 8 4V4c0-1.1-.9-2-2-2H6zm0 2h12v15.17l-6-3-6 3V4z" />
-          </svg>
-        </button>
+        <div className={styles.bookmarkContainer}>
+          <button type="button" className={styles.bookmark}>
+            <svg
+              aria-hidden="true"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+              fill="currentColor"
+            >
+              <path d="M6 2c-1.1 0-2 .9-2 2v18l8-4 8 4V4c0-1.1-.9-2-2-2H6zm0 2h12v15.17l-6-3-6 3V4z" />
+            </svg>
+          </button>
+        </div>
       </nav>
     </header>
   );
