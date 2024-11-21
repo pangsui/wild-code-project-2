89fe1e9b7a8f1ad882f3de418cdb67c172064406
pangsui <upangsuilingeh@gmail.com>
2024-11-21 14:53:48 +0100

    [Feat]:loader and error handling added to all heros page

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,28 +2,28 @@ import { NavLink } from "react-router-dom";
 import styles from "./Navbar.module.css";
 
 export default function Navbar() {
-	return (
-		<header className={styles.header}>
-			<div className={styles.logoContent}>
-				<NavLink to="/">
-					<img src="images/logo.png" alt="Logo" className={styles.headerLogo} />
-				</NavLink>
-				<NavLink to="/">
-					{" "}
-					<h1> HERODEX</h1>
-				</NavLink>
-				<div className={styles.allSections}>
-					<NavLink to="/details" className={styles.link}>
-						Explore heroes!
-					</NavLink>
-					<NavLink to="/allHeroes" className={styles.link}>
-						All super heroes
-					</NavLink>
-					<NavLink to="/fevorites" className={styles.link}>
-						Fevorites
-					</NavLink>
-				</div>
-			</div>
-		</header>
-	);
+  return (
+    <header className={styles.header}>
+      <div className={styles.logoContent}>
+        <NavLink to="/">
+          <img src="images/logo.png" alt="Logo" className={styles.headerLogo} />
+        </NavLink>
+        <NavLink to="/">
+          {" "}
+          <h1> HERODEX</h1>
+        </NavLink>
+        <div className={styles.allSections}>
+          <NavLink to="/details" className={styles.link}>
+            Explore heroes!
+          </NavLink>
+          <NavLink to="/allHeroes" className={styles.link}>
+            All super heroes
+          </NavLink>
+          <NavLink to="/fevorites" className={styles.link}>
+            Fevorites
+          </NavLink>
+        </div>
+      </div>
+    </header>
+  );
 }
